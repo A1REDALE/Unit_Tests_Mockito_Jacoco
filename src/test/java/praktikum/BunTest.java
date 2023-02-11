eebd57968a060dbe62a52969f8a43ae7f06e7ca8
Evgeniy Storchak <EStorchak@yandex.ru>
2023-02-11 17:39:31 +0300

    1stcommit

---
+++ src/test/java/praktikum/BunTest.java
@@ -0,0 +1,14 @@
+package praktikum;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BunTest {
+
+    @Test
+    public void testBunConstructor() {
+        Bun bun = new Bun("test", 103.0f);
+        assertEquals("test", bun.getName());
+        assertEquals(103.0f, bun.getPrice(), 0.0f);
+    }
+}
+
